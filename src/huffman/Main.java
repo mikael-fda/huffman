9a61bdfd41491c106be9530c4e3a6651a9cf2e36
mikael <mikael.almeida@hotmail.fr>
2021-05-15 14:59:17 +0200

    infite option chooser

--- src/huffman/Main.java
+++ src/huffman/Main.java
@@ -31,7 +31,8 @@ public class Main {
 		String[] choices = {"Quitter", "Décoder un texte", "Coder un texte"};
 
 
-		int res = JOptionPane.showOptionDialog(null,
+		while(true){
+			int res = JOptionPane.showOptionDialog(null,
 				"Que voulez-vous faire ?", 
 				"Algorithme de Huffman", 
 				JOptionPane.YES_NO_CANCEL_OPTION, 
@@ -40,52 +41,43 @@ public class Main {
 				choices, 
 				choices[0]
 			);
-		
-		if(res == 2) {
-			path.setDialogTitle("Choisis un fichier à coder");
-			
-			if(path.showOpenDialog(null) != JFileChooser.APPROVE_OPTION ) {
-				System.exit(1);
-			}
-			// Encoding 
-			FileReaderEncode fre = new FileReaderEncode(path.getSelectedFile().getAbsolutePath());
-			Element[] freq = fre.getEncodings();
-			// Arbre de Huffman
-			BinaryTree huffman = BinaryTree.huffman(freq);
-			
-			// Write file
-			FileWritterEncode fwe = new FileWritterEncode(fre.getFilePath(), huffman, fre);
-			
-			
-		}
-		else if(res == 1) {
-			path.setDialogTitle("Choisis un fichier à décoder");
-			path.addChoosableFileFilter(new FileFilter() {
+
+			if(res == 2) {
+				path.setDialogTitle("Choisis un fichier à coder");
 				
-				@Override
-				public String getDescription() {
-					// TODO Auto-generated method stub
-					return "huffman encoded (.huf)";
+				if(path.showOpenDialog(null) != JFileChooser.APPROVE_OPTION ) {
+					continue;
 				}
+				// Encoding 
+				FileReaderEncode fre = new FileReaderEncode(path.getSelectedFile().getAbsolutePath());
+				Element[] freq = fre.getEncodings();
+				// Arbre de Huffman
+				BinaryTree huffman = BinaryTree.huffman(freq);
 				
-				@Override
-				public boolean accept(File f) {
-					if(f.isDirectory()) return true;
-					return f.getName().endsWith(".huf");
-				}
-			});
-			if(path.showOpenDialog(null) != JFileChooser.APPROVE_OPTION ) {
-				System.exit(1);
+				// Write file
+				FileWritterEncode fwe = new FileWritterEncode(fre.getFilePath(), huffman, fre);
+				
+				
 			}
-			// Read file encoded
-			FileReaderDecode frd = new FileReaderDecode(path.getSelectedFile().getAbsolutePath());
-			frd.readFile();
-			
-			// Write file decoded
-			FileWritterDecode fwd = new FileWritterDecode(frd);
-			fwd.writeFile();
+			else if(res == 1) {
+				path.setDialogTitle("Choisis un fichier à décoder");
+	
+				if(path.showOpenDialog(null) != JFileChooser.APPROVE_OPTION ) {
+					continue;
+				}
+				// Read file encoded
+				FileReaderDecode frd = new FileReaderDecode(path.getSelectedFile().getAbsolutePath());
+				frd.readFile();
+				
+				// Write file decoded
+				FileWritterDecode fwd = new FileWritterDecode(frd);
+				fwd.writeFile();
+
+			}
+			else if(res == 0 || res == -1){
+				System.exit(0);
+			}
 
 		}
-
 	}
 }
